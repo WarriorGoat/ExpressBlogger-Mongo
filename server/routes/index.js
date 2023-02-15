64c8f22466029781c31f7b0aa4e8f5371507e867
WarriorGoat <michael.d.spitz@gmail.com>
2023-02-15 11:17:56 -0500

    cleaned up some syntax.

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,36 +12,6 @@ try {
 } catch (error) {
   console.log(error);
 }
-})
-
-//add a new post
-indexRouter.post("/create-one", async (req, res, next) => {
-  try {
-    //parse out fields from post request
-    const title = req.body.title;
-    const author = req.body.author;
-    const text = req.body.text;
-    const categories = req.body.categories;
-  
-    //pass fields to model
-    const newBlogEntry = new Blog({
-      title,
-      author,
-      text,
-      categories
-    });
-
-    //save to database
-    const savedData = await newBlogEntry.save();
-
-    //return the successful request to the user
-    res.json({
-      success: true,
-      blogs: savedData
-    })
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 module.exports = indexRouter;
